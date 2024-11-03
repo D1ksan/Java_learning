bf1e39bf4107b698c58a6dce6718fe13b0b6fd6a
D1ksan <ekhestt@gmail.com>
2024-11-03 16:56:38 +0300

    task2 with gallons->liters complete

---
+++ GaltoLit.java
@@ -0,0 +1,9 @@
+class GaltoLit{
+    public static void main(String args[]){
+
+        double gallons = 10;
+        double liters = gallons*3.7854;
+        System.out.println(gallons + " галлонам соответсвует " + liters+ " литров");
+
+    }
+}
