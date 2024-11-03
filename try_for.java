36e5236a9ca714d2c1502d8a5633b2fe929e8045
D1ksan <ekhestt@gmail.com>
2024-11-03 17:06:05 +0300

    trying cycle for with mini count

---
+++ try_for.java
@@ -0,0 +1,8 @@
+public class try_for {
+    public static void main(String args[]){
+        int count;
+        for (count=0; count <5; count ++)
+            System.out.println(count);
+        System.out.println("Стопэ");
+    }
+}
