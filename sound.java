f6506d1289c8ed5d6d5de32b21c3358d6def68c3
D1ksan <ekhestt@gmail.com>
2024-11-04 20:19:11 +0300

    task2: easy-level

---
+++ sound.java
@@ -0,0 +1,9 @@
+//упражнение 2.1 программа, вычисляющая расстояние в футах до источника звука из книги
+public class sound {
+    public static void main(String args[]){
+        double t = 7.2;
+        double v = 1100;
+        System.out.println("fgdfg"+t*v);
+
+    }
+}
