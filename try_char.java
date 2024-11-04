ea90d2cd170971e69c00be19dc2508e24a3bdc24
D1ksan <ekhestt@gmail.com>
2024-11-04 20:05:21 +0300

    test: trying type-char

---
+++ try_char.java
@@ -0,0 +1,12 @@
+public class try_char {
+    public static void main(String args[]){
+        char ch;
+        ch = 'M';
+        System.out.println(ch);
+        ch++;
+        System.out.println(ch);
+        ch = 90; // в наборе символов ASCII 90==Z
+        System.out.println(ch);
+
+    }
+}
