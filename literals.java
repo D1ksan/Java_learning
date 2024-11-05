e6a68b2e96ab8590a17479bc7d070862a9fbf462
D1ksan <ekhestt@gmail.com>
2024-11-05 23:18:41 +0300

    learn: try literals

---
+++ literals.java
@@ -0,0 +1,8 @@
+public class literals{
+    public static void main(String args[]){
+        System.out.println("'fdf'");// пробую одинарные кавычки
+        System.out.println("\"кавычки в кавычках\"");//кавычки в выводе, кавычки в кавычках
+        System.out.println("Первая строка\nВторая строка");
+        System.out.println("A\tB\tC"); //табуляция в строке 
+    }
+}
