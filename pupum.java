e5635430541798b9c8b7a41b4ebea28b5bc02cf4
D1ksan <ekhestt@gmail.com>
2024-11-06 14:30:53 +0300

    experiments: output a type of c - impossible

---
+++ pupum.java
@@ -0,0 +1,11 @@
+public class pupum{
+    public static void main(String args[]){
+        double x,y;
+        x = 5;
+        y=2;
+        double c;
+        c = (int)x/y;
+        System.out.println(c);
+        System.out.println();
+    }
+}
