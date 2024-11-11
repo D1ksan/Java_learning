2db4927278054f3409cac0ec5211374359c44afb
D1ksan <ekhestt@gmail.com>
2024-11-11 22:39:26 +0300

    Complete: micro-mini game GUESS SYMBOOOOL

---
+++ guess_symbol.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class guess_symbol {
+    public static void main(String args[]) throws java.io.IOException{
+        Random random = new Random();
+        char ch, answer;
+        answer = (char) ('A'+ random.nextInt(26));
+        System.out.println(answer);
+        System.out.println("Угадайте букву от A до Z которую я загадал: (после ввода нажмите Enter) ");
+        ch = (char) System.in.read();
+        if (ch == answer){
+            System.out.println("Вы угадали! Поздравляем!!!");
+        }
+        else {
+            System.out.println("К сожалению, вы не угадали, попробуйте еще!");
+        }
+    }
+}
